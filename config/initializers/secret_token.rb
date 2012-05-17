e8bded6c428b979a3b756824a3ce0b890e3b1aee
none <alexander.podkidyshev@gmail.com>
2012-05-17 11:04:21 +0300

    Каркас приложения

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxAudioUpload::Application.config.secret_token = '1173b849d716dccfba582e7c0a4a2d495918c5afb4e0807c532fbff9c8ad76bb106eb319534f5d2227ba1b90cbfa79399ef1aa023f0f4eebefd431a519a08a64'
